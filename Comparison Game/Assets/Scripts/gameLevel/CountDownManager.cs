6043b18cec35fede94546d0457de95011b402d9f
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-01-31 16:36:57 +0300

    game has started

---
+++ Comparison Game/Assets/Scripts/gameLevel/CountDownManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CountDownManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject countDownImage;
+
+    [SerializeField]
+    private Text countDownText;
+
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = Object.FindObjectOfType<GameManager>();
+
+    }
+    void Start()
+    {
+        StartCoroutine(countDownRoutine());
+
+    }
+
+    IEnumerator countDownRoutine()
+    {
+
+        countDownText.text = "3";
+        yield return new WaitForSeconds(0.5f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(1.0f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.5f);
+
+        countDownText.text = "2";
+        yield return new WaitForSeconds(0.5f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(1.0f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.5f);
+        countDownText.text = "1";
+        yield return new WaitForSeconds(0.5f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(1, 0.5f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(1.0f);
+        countDownImage.transform.GetComponent<RectTransform>().DOScale(0, 0.5f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.5f);
+
+        StopAllCoroutines();
+
+
+        gameManager.StartGame();
+
+
+    }
+}
---
+++ Comparison Game/Assets/Scripts/gameLevel/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scoreTimePanel;
+
+    [SerializeField]
+    private GameObject collectScoreImageText;
+
+    [SerializeField]
+    private GameObject topRectangleImage, bottomRectangleImage;
+
+    [SerializeField]
+    private Text topRectangleText, bottomRectangleText;
+
+
+
+    void Start()
+    {
+        topRectangleText.text = "";
+        bottomRectangleText.text = "";
+
+        updateSceneScreen();
+    }
+
+    void updateSceneScreen()
+    {
+
+        scoreTimePanel.GetComponent<CanvasGroup>().DOFade(1, 1f);
+        collectScoreImageText.GetComponent<CanvasGroup>().DOFade(1, 1f);
+
+
+        topRectangleImage.transform.GetComponent<RectTransform>().DOLocalMoveX(0, 0.7f);
+        bottomRectangleImage.transform.GetComponent<RectTransform>().DOLocalMoveX(0, 0.7f);
+
+
+    }
+
+    public void StartGame()
+    {
+        topRectangleText.text= "(40+40)-25";
+        bottomRectangleText.text = "(5+55)/2";
+        
+        Debug.Log("Start Game");
+    }
+}
