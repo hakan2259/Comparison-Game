5a968ee21dec608c4423ae27cebf3a22b8bbf8f4
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-01-31 15:18:27 +0300

    MenuLevel scene completed

---
+++ Comparison Game/Assets/Scripts/menuLevel/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform brain;
+
+    [SerializeField]
+    private Transform startButton;
+
+    void Start()
+    {
+        brain.transform.GetComponent<RectTransform>().DOLocalMoveX(0f,1f).SetEase(Ease.OutBack);
+        startButton.transform.GetComponent<RectTransform>().DOLocalMoveY(-250f,1f).SetEase(Ease.OutBack);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("GameLevel");
+    }
+}
